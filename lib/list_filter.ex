f1a15e22d034c56211c9c503be4eb9374622131a
Diego Novais <contato@diegonovais.com.br>
2021-04-30 19:13:36 -0300

    filter all numeric elements in the list and return how many odd numbers there are

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,46 @@
+require Integer
+
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
+  def call(list) do
+    list
+    |> Enum.map(fn element -> get_numbers(element) end)
+    |> Enum.reject(&is_nil/1)
+    |> length()
+  end
 
-  @doc """
-  Hello world.
+  defp get_numbers(element) do
+    element
+    |> parser_element()
+    |> parser_number()
+    |> get_odd_number()
+  end
 
-  ## Examples
+  defp parser_element(element) when is_number(element) do
+    element
+  end
 
-      iex> ListFilter.hello()
-      :world
+  defp parser_element(element) do
+    element
+    |> Integer.parse()
+  end
 
-  """
-  def hello do
-    :world
+  defp parser_number({number, _}) do
+    number
+  end
+
+  defp parser_number(number) when is_number(number) do
+    number
+  end
+
+  defp parser_number(:error) do
+    0
+  end
+
+  defp get_odd_number(number) when Integer.is_odd(number) do
+    number
+  end
+
+  defp get_odd_number(_number) do
+    nil
   end
 end
